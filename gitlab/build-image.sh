aee524a7c91077b687a24a3a9bf61eb44e4f5953
Oleksii Khilkevych <oleksiy@noroutine.me>
2020-09-20 18:20:32 +0200

    Cosmetic fixes

--- gitlab/build-image.sh
+++ gitlab/build-image.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -eux
 
 build_image() {
-	local component=${1:-}
-	local target=${2:-}
-	shift 2
+    local component=${1:-}
+    local target=${2:-}
+    shift 2
 
-	local docker_build_args=${@:-}
+    local docker_build_args=${@:-}
 
-	test ! -z ${component}
-	test ! -z ${target}
+    test ! -z ${component}
+    test ! -z ${target}
 
-	test ! -z ${DOCKER_HUB:-}
+    test ! -z ${DOCKER_HUB:-}
 
-	local implicit_args="--build-arg HTTP_PROXY \
-		--build-arg HTTPS_PROXY \
-		--build-arg NO_PROXY \
-		--build-arg DOCKER_HUB \
-		--build-arg DOCKER_NAMESPACE"
+    local implicit_args="--build-arg HTTP_PROXY \
+        --build-arg HTTPS_PROXY \
+        --build-arg NO_PROXY \
+        --build-arg DOCKER_HUB \
+        --build-arg DOCKER_NAMESPACE"
 
-	echo "Building ${component} as ${target}"
+    echo "Building ${component} as ${target}"
 
-	docker build ${implicit_args} ${docker_build_args} -t ${DOCKER_HUB}/${target} docker/${component}
-	docker push ${DOCKER_HUB}/${target}
+    docker build ${implicit_args} ${docker_build_args} -t ${DOCKER_HUB}/${target} docker/${component}
+    docker push ${DOCKER_HUB}/${target}
 }
 
 _main() {
     local source_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     source ${source_dir}//env.sh
-	
-	build_image "${@}"
+    
+    build_image "${@}"
 }
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || _main "${@:-}"
