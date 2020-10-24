ab7ed050478eae2eccae8e38dd4f6d11c00a6f08
Oleksii Khilkevych <oleksiy@noroutine.me>
2020-10-24 12:25:15 +0200

    Remove env version for hashitools, not used anyway

--- docker/builder/setup.d/00_vars.sh
+++ docker/builder/setup.d/00_vars.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash -eux
 
-export NOMAD_VERSION=${NOMAD_VERSION:-0.10.0}
-export TERRAFORM_VERSION=${TERRAFORM_VERSION:-0.12.18}
-export CONSUL_VERSION=${CONSUL_VERSION:-1.6.1}
-export VAULT_VERSION=${VAULT_VERSION:-1.2.3}
-export ENVCONSUL_VERSION=${ENVCONSUL_VERSION:-0.9.0}
-export CONSUL_TEMPLATE_VERSION=${CONSUL_TEMPLATE_VERSION:-0.22.0}
-
 export HTTP_PROXY=http://bo01-vm-proxy01.node.bo01.noroutine.me:3128
 export HTTPS_PROXY=http://bo01-vm-proxy01.node.bo01.noroutine.me:3128
 export NO_PROXY="*.noroutine.me,localhost,127.0.0.1"
