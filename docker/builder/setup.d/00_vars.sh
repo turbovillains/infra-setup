5d05aa4e9940d1dd07dbba59e32c216e9130a1a3
Oleksii <oleksiy@noroutine.me>
2021-08-03 21:52:13 +0200

    Squid

--- docker/builder/setup.d/00_vars.sh
+++ docker/builder/setup.d/00_vars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash -eux
 
-export HTTP_PROXY=http://bo01-vm-proxy01.node.bo01.noroutine.me:3128
-export HTTPS_PROXY=http://bo01-vm-proxy01.node.bo01.noroutine.me:3128
-export NO_PROXY="*.noroutine.me,localhost,127.0.0.1"
+export HTTP_PROXY=http://proxy.bo01.noroutine.me:3128
+export HTTPS_PROXY=http://proxy.bo01.noroutine.me:3128
+export NO_PROXY="*.noroutine.me,10.0.0.0/8,localhost,127.0.0.1"
