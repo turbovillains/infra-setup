9dd15c41ed046216c4043a4b48f034bde064806c
Oleksii <oleksiy@noroutine.me>
2022-04-18 07:51:45 +0200

    Updates, use node lts

--- docker/builder/setup.d/04_setup_nvm.sh
+++ docker/builder/setup.d/04_setup_nvm.sh
@@ -17,6 +17,6 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-nvm install stable
+nvm install 'lts/*' --latest-npm
 npm install -g npm yarn degit
 EOF
