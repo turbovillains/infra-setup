97b6707ed0ae4774cbc2124385e4b358803e5215
Oleksii <oleksiy@noroutine.me>
2022-05-05 01:21:10 +0200

    Bitnami Grafana and Grafana images with our dashboards

---
+++ docker/bitnami-grafana/README.md
@@ -0,0 +1,4 @@
+# Dashboards
+
+This docker image contains several dashboard sets inside.
+To use specifc dashboard set, set the `GF_DASHBOARDS_SET` env variable
---
+++ docker/bitnami-grafana/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eux
+
+grafana-cli plugins install abhisant-druid-datasource
+grafana-cli plugins install grafana-worldmap-panel
+grafana-cli plugins install grafana-piechart-panel
+grafana-cli plugins install briangann-gauge-panel
+grafana-cli plugins install savantly-heatmap-panel
+grafana-cli plugins install mtanda-histogram-panel
+grafana-cli plugins install michaeldmoore-annunciator-panel
+grafana-cli plugins install zuburqan-parity-report-panel
+grafana-cli plugins install petrslavotinek-carpetplot-panel
+grafana-cli plugins install ryantxu-ajax-panel
+grafana-cli plugins install ryantxu-annolist-panel
+grafana-cli plugins install snuids-radar-panel
+grafana-cli plugins install snuids-trafficlights-panel
+grafana-cli plugins install jdbranham-diagram-panel
+# grafana-cli plugins install devopsprodigy-kubegraf-app
+grafana-cli plugins install michaeldmoore-multistat-panel
+grafana-cli plugins install scadavis-synoptic-panel
+grafana-cli plugins install goshposh-metaqueries-datasource
+grafana-cli plugins install xginn8-pagerduty-datasource
+grafana-cli plugins install flant-statusmap-panel
+grafana-cli plugins install pixie-pixie-datasource
+
+# End of file
