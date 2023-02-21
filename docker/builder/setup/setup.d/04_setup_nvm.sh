87af8b07d06a5a775639b785c8454ea5a9a0b58e
Oleksii Khilkevych <oleksiy@noroutine.me>
2023-02-21 11:07:56 +0100

    Updates

--- docker/builder/setup/setup.d/04_setup_nvm.sh
+++ docker/builder/setup/setup.d/04_setup_nvm.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash -eux
 
-cat << EOF | tee /home/${BUILDER_USER}/.npmrc
-registry=https://nexus.nrtn.dev/repository/npm/
-# strict-ssl=false
-EOF
+# cat << EOF | tee /home/${BUILDER_USER}/.npmrc
+# registry=https://nexus.nrtn.dev/repository/npm/
+# # strict-ssl=false
+# EOF
 
-cat << EOF | tee /home/${BUILDER_USER}/.yarnrc
-registry "https://nexus.nrtn.dev/repository/npm/"
-# strict-ssl false
-EOF
+# cat << EOF | tee /home/${BUILDER_USER}/.yarnrc
+# registry "https://nexus.nrtn.dev/repository/npm/"
+# # strict-ssl false
+# EOF
 
 sudo -H -u ${BUILDER_USER} bash -s <<'EOF'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"
 [ -s "${NVM_DIR}/bash_completion" ] && source "${NVM_DIR}/bash_completion"
