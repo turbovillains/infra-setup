6154254e9a5129decb4817529fe276d3481f66f3
Oleksii Khilkevych <oleksiy@noroutine.me>
2023-12-18 14:02:55 +0100

    Updates

--- docker/builder/setup/setup.d/03_tools.sh
+++ docker/builder/setup/setup.d/03_tools.sh
@@ -5,23 +5,23 @@ sudo curl -Lo /usr/local/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s htt
 
 # trivy
 # https://github.com/aquasecurity/trivy/releases
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.47.0
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.48.1
 
 # pack
 # https://github.com/buildpacks/pack/releases
-(curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.31.0/pack-v0.31.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
+(curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.32.1/pack-v0.32.1-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
 
 # helm
 # https://github.com/helm/helm/releases
-(curl -sSL "https://get.helm.sh/helm-v3.13.1-linux-amd64.tar.gz" | sudo tar -C /usr/local/bin/ --no-same-owner --strip-components=1 -xzv linux-amd64/helm)
+(curl -sSL "https://get.helm.sh/helm-v3.13.3-linux-amd64.tar.gz" | sudo tar -C /usr/local/bin/ --no-same-owner --strip-components=1 -xzv linux-amd64/helm)
 
 # skaffold
 # https://github.com/GoogleContainerTools/skaffold/releases
-sudo curl -sLo /usr/local/bin/skaffold https://storage.googleapis.com/skaffold/releases/v2.8.0/skaffold-linux-amd64 && sudo chmod +x /usr/local/bin/skaffold
+sudo curl -sLo /usr/local/bin/skaffold https://storage.googleapis.com/skaffold/releases/v2.9.0/skaffold-linux-amd64 && sudo chmod +x /usr/local/bin/skaffold
 
 # yq
 # https://github.com/mikefarah/yq/releases
-sudo curl -sLo /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.35.2/yq_linux_amd64 && sudo chmod +x /usr/local/bin/yq
+sudo curl -sLo /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.40.5/yq_linux_amd64 && sudo chmod +x /usr/local/bin/yq
 
 # yj
 # https://github.com/sclevine/yj/releases
@@ -29,33 +29,33 @@ sudo curl -sLo /usr/local/bin/yj https://github.com/sclevine/yj/releases/downloa
 
 # jq
 # https://github.com/stedolan/jq/releases
-sudo curl -sLo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.7/jq-linux64 && sudo chmod +x /usr/local/bin/jq
+sudo curl -sLo /usr/local/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.7.1/jq-linux64 && sudo chmod +x /usr/local/bin/jq
 
 # ytt
 # https://github.com/vmware-tanzu/carvel-ytt/releases
-sudo curl -sLo /usr/local/bin/ytt https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.46.2/ytt-linux-amd64 && sudo chmod +x /usr/local/bin/ytt
+sudo curl -sLo /usr/local/bin/ytt https://github.com/vmware-tanzu/carvel-ytt/releases/download/v0.46.3/ytt-linux-amd64 && sudo chmod +x /usr/local/bin/ytt
 
 # kustomize
 # https://github.com/kubernetes-sigs/kustomize/releases
-(curl -sSL "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.2.1/kustomize_v5.2.1_linux_amd64.tar.gz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv kustomize)
+(curl -sSL "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.3.0/kustomize_v5.3.0_linux_amd64.tar.gz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv kustomize)
 
 # argocd
 # https://github.com/argoproj/argo-cd/releases
-sudo curl -sLo /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v2.9.0/argocd-linux-amd64 && sudo chmod +x /usr/local/bin/argocd
+sudo curl -sLo /usr/local/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v2.9.3/argocd-linux-amd64 && sudo chmod +x /usr/local/bin/argocd
 
 # argo workflows
 # https://github.com/argoproj/argo-workflows/releases
-curl -sLo argocli.gz https://github.com/argoproj/argo-workflows/releases/download/v3.5.1/argo-linux-amd64.gz
+curl -sLo argocli.gz https://github.com/argoproj/argo-workflows/releases/download/v3.5.2/argo-linux-amd64.gz
 gunzip argocli.gz
 sudo mv argocli /usr/local/bin/argocli
 
 # https://github.com/goreleaser/goreleaser/releases
-curl -sLo- https://github.com/goreleaser/goreleaser/releases/download/v1.21.2/goreleaser-1.21.2-1-x86_64.pkg.tar.zst \
+curl -sLo- https://github.com/goreleaser/goreleaser/releases/download/v1.22.1/goreleaser-1.22.1-1-x86_64.pkg.tar.zst \
   | sudo tar -C /usr/local/bin/ --no-same-owner --strip-components=2 --use-compress-program=unzstd -xv usr/bin/goreleaser
 
 # terraform
 # https://releases.hashicorp.com/terraform
-curl -sLo terraform.zip https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
+curl -sLo terraform.zip https://releases.hashicorp.com/terraform/1.6.6/terraform_1.6.6_linux_amd64.zip
 unzip terraform.zip
 chmod +x terraform
 sudo mv terraform /usr/local/bin/terraform
