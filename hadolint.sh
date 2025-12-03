e74a8e0e8253c0b4d5eccf719f24458680fd32cd
Oleksii Khilkevych <oleksiy@noroutine.me>
2025-12-03 06:29:16 +0100

    Cleanup legacy stuff

--- hadolint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for dockerfile in docker/*/Dockerfile; do
-  docker run --rm -i cr.noroutine.me/infra/hadolint:${INFRA_VERSION} < ${dockerfile};
-done
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from infra-setup!")
-
-
-if __name__ == "__main__":
-    main()
